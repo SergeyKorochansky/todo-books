1119d650a36a83c4927e4f05eba11e7dcec6d96f
Serhij Korocahnskyj <webzepter@gmail.com>
2014-07-09 21:50:48 +0300

    Remove unused method in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,12 +11,4 @@ class Book < ActiveRecord::Base
     self.read_pages  ||= 0
     self.total_pages ||= 0
   end
-  #
-  # def progress
-  #   if total_pages.zero?
-  #     nil
-  #   else
-  #     ((read_pages.to_f / total_pages.to_f) * 100).to_i
-  #   end
-  # end
 end
