8eb63fca6e4c58d99e6c52ca053371850980f489
Serhij Korocahnskyj <webzepter@gmail.com>
2014-07-10 08:54:35 +0300

    Add Groups controller and routes

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,48 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :update, :destroy]
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def create
+    @book = Group.new(group_params)
+    respond_to do |format|
+      if @group.update(group_params)
+        format.json { render :show }
+      else
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.json { render :show }
+      else
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(group_params[:id])
+  end
+
+  def group_params
+    params.permit(:id, :name, :url)
+  end
+end
