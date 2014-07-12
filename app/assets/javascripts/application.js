cebc7e967d2669145731a73f1d0c2e23cd8b3a49
Serhij Korocahnskyj <webzepter@gmail.com>
2014-07-12 23:23:54 +0300

    Add multi-select to view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,8 @@
 //= require ng-rails-csrf
 //= require angular-ui-bootstrap
 //= require angular-ui-bootstrap-tpls
+//= require angular-multi-select
 //= require_self
 //= require_tree .
 
-var booksApp = angular.module('booksApp', ['ngResource', 'ng-rails-csrf', 'ui.bootstrap']);
+var booksApp = angular.module('booksApp', ['ngResource', 'ng-rails-csrf', 'ui.bootstrap', 'multi-select']);
